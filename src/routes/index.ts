c3dca79dba53de1f1a6e651d81b4d60cecf66abb
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-10-12 23:11:49 +0200

    Add default route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,6 @@ import { productsRouter } from "./products";
 export const appRouter = express.Router();
 
 appRouter.use("/products", productsRouter);
+appRouter.get("/", (_req, res) => {
+  res.json({ test: "test route" });
+});
