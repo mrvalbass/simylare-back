d38ee4d6dd6f4ace3deb5558fe5cd7dfec35193e
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-10-13 09:44:46 +0200

    debugging deployment on vercel

--- dist/routes/products/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productsRouter = void 0;
-const express_1 = require("express");
-const createProduct_1 = require("./createProduct");
-exports.productsRouter = (0, express_1.Router)();
-exports.productsRouter.post("/createProduct", (0, createProduct_1.createProduct)());
-//# sourceMappingURL=index.js.map
