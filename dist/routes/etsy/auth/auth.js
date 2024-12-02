c7b124623afa665ecad664f9842fb8f3a5668d6c
ValentinG <valentin.guillot@stellia.ai>
2024-12-03 00:33:28 +0100

    bug fix

--- dist/routes/etsy/auth/auth.js
+++ dist/routes/etsy/auth/auth.js
@@ -3,14 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.mwAuth = mwAuth;
 function mwAuth(req, res) {
     try {
-        const url = `https://www.etsy.com/oauth/connect?
-    response_type=code
-    &redirect_uri=${process.env.REDIRECT_URI}
-    &scope=listings_r
-    &client_id=${process.env.CLIENT_ID}
-    &state=${req.state}
-    &code_challenge=${req.codeChallenge}
-    &code_challenge_method=S256`;
+        const url = `https://www.etsy.com/oauth/connect?response_type=code
+&redirect_uri=${process.env.REDIRECT_URI}
+&scope=listings_r
+&client_id=${process.env.CLIENT_ID}
+&state=${req.state}
+&code_challenge=${req.codeChallenge}
+&code_challenge_method=S256`;
         res.redirect(url);
     }
     catch (e) {
--- src/routes/etsy/auth/auth.ts
+++ src/routes/etsy/auth/auth.ts
@@ -3,14 +3,13 @@ import { AuthRequest } from ".";
 
 export function mwAuth(req: AuthRequest, res: Response) {
   try {
-    const url = `https://www.etsy.com/oauth/connect?
-    response_type=code
-    &redirect_uri=${process.env.REDIRECT_URI}
-    &scope=listings_r
-    &client_id=${process.env.CLIENT_ID}
-    &state=${req.state}
-    &code_challenge=${req.codeChallenge}
-    &code_challenge_method=S256`;
+    const url = `https://www.etsy.com/oauth/connect?response_type=code
+&redirect_uri=${process.env.REDIRECT_URI}
+&scope=listings_r
+&client_id=${process.env.CLIENT_ID}
+&state=${req.state}
+&code_challenge=${req.codeChallenge}
+&code_challenge_method=S256`;
     res.redirect(url);
   } catch (e: unknown) {
     console.error(e);
