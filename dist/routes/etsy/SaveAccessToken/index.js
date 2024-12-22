bcd704e36e73833c3a2cd949c739d1b046806d87
ValentinG <valentin.guillot@stellia.ai>
2024-12-22 19:16:07 +0100

    test

--- dist/routes/etsy/SaveAccessToken/index.js
+++ dist/routes/etsy/SaveAccessToken/index.js
@@ -3,10 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.saveAccessToken = void 0;
 const checkState_1 = require("./checkState");
 const getAccessToken_1 = require("../../../middleware/getAccessToken");
-const __1 = require("..");
-const saveAccessToken = () => [
-    checkState_1.mwCheckState,
-    (0, getAccessToken_1.mwGetAccessToken)(__1.codeVerifier),
-];
+const saveAccessToken = () => [checkState_1.mwCheckState, (0, getAccessToken_1.mwGetAccessToken)()];
 exports.saveAccessToken = saveAccessToken;
 //# sourceMappingURL=index.js.map
--- src/routes/etsy/SaveAccessToken/index.ts
+++ src/routes/etsy/SaveAccessToken/index.ts
@@ -1,8 +1,4 @@
 import { mwCheckState } from "./checkState";
 import { mwGetAccessToken } from "../../../middleware/getAccessToken";
-import { codeVerifier } from "..";
 
-export const saveAccessToken = () => [
-  mwCheckState,
-  mwGetAccessToken(codeVerifier),
-];
+export const saveAccessToken = () => [mwCheckState, mwGetAccessToken()];
